22a15a6aacb022a1b9c6b1c00b2940cec91ae9c3
Tens1des <romankotov3213@gmail.com>
2025-10-07 15:19:29 +0300

    Initial Commit

---
+++ DayCounter/DayCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCounterApp.swift
+//  DayCounter
+//
+//  Created by Рома Котов on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DayCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
